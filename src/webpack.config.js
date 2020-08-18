266b04f928dba4b5f0850cabe16c6ae5d3898dfa
Camora28@ <camora88@gmail.com>
2020-08-18 06:03:56 -0500

    corregi problema de copilacion webpack por moviendo archivo html a la carpeta src

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,19 +1,19 @@
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const path = require('path');
 
 module.exports = {
     entry: './src/app/index.js',
     output: {
-        path: path.join(__dirname,'dist'),
+        path: __dirname + '/dist',
         filename: 'bundle.js'
     },
     // devServer:{
     //     port: 3000
     // },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+    // plugins:[
+    //     new HtmlWebpackPlugin({
+    //         template: './src/index.html'
+    //     })
+    // ]
 }
