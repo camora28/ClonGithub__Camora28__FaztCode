e2dc46b8307f62c8180db32b56afb71626373d27
Camora28@ <camora88@gmail.com>
2020-08-18 05:40:30 -0500

    corriendo error delete /module y package.json

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,19 @@
-module.export = {
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
     entry: './src/app/index.js',
     output: {
-        path: __dirname +'/dist',
+        path: path.join(__dirname,'dist'),
         filename: 'bundle.js'
-    }
+    },
+    // devServer:{
+    //     port: 3000
+    // },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
 }
